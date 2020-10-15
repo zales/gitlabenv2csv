5be2184f309ce957028520159bcaf7a9bcf8b82e
Ondrej Zalesky <o.zalesky@gmail.com>
2020-10-15 04:36:59 +0200

    added data source validation

--- gitlabenv2csv.py
+++ gitlabenv2csv.py
@@ -2,9 +2,69 @@
 
 import sys
 from datetime import datetime
+import re
+import logging
 import gitlab
 import configargparse
 import pandas as pd
+from pandas_schema.validation import CustomElementValidation
+import pandas_schema
+from pandas_schema import Column
+import numpy as np
+
+
+
+pd.set_option('display.max_colwidth', 120)
+
+
+def check_boolean(boolean):
+
+    if isinstance(boolean, bool):
+        return True
+    return False
+
+
+def check_key(key):
+
+    if re.match("^[A-Za-z0-9_-]*$", key):
+        return True
+    return False
+
+
+def check_variable_type(variable_type):
+
+    if variable_type in {'env_var', 'koko'}:
+        return True
+    return False
+
+
+def data_validation(data):
+
+    # define validation elements
+    key_validation = [CustomElementValidation(lambda k: check_key(k), 'is not valid key - only letters, digits and _')]
+    bool_validation = [CustomElementValidation(lambda b: check_boolean(b), 'is not boolean')]
+    null_validation = [CustomElementValidation(lambda d: d is not np.nan, 'this field cannot be null')]
+    variable_type_validation = [CustomElementValidation(lambda t: check_variable_type(t), 'is not valid variable_type')]
+
+
+    # variable_type,key,value,protected,masked
+    # define validation schema
+    schema = pandas_schema.Schema([
+        Column('variable_type', variable_type_validation + null_validation),
+        Column('key', key_validation + null_validation),
+        Column('value', null_validation),
+        Column('protected', bool_validation + null_validation),
+        Column('masked', bool_validation + null_validation)])
+
+    # apply validation
+    errors = schema.validate(data)
+    errors_index_rows = [e.row for e in errors]
+
+    if not errors_index_rows:
+        return True
+    else:
+        logging.error(pd.DataFrame({'errors':errors}))
+        return False
 
 
 def gitlab_env_to_csv(element, file_path):
@@ -16,11 +76,22 @@ def gitlab_env_to_csv(element, file_path):
 
     # ENV variables to csv file
     df = pd.DataFrame(test, columns=['variable_type', 'key', 'value', 'protected', 'masked'])
-    print(df)
+    logging.info(df)
     df.to_csv(file_path, index=False)
 
 
 def csv_to_gitlab_env(element, file_path='dict.csv'):
+
+    # Parse csv file
+    df = pd.read_csv(file_path, index_col=False)
+
+    # validate csv file
+    if data_validation(df):
+        logging.info("Data validation passed.")
+    else:
+        logging.error("Data validation failed. Please check csv file.")
+        sys.exit(123)
+
 
     # Backup current ENV variables
     repo_name = element.name
@@ -32,10 +103,6 @@ def csv_to_gitlab_env(element, file_path='dict.csv'):
     # Delete all ENV variables in group/project
     for env in element.variables.list(as_list=False):
         element.variables.delete(env.key)
-
-    # Parse csv file
-    df = pd.read_csv(file_path, index_col=False)
-    print(df)
 
     # Create ENV variables
     for row in df.itertuples(index=True):
